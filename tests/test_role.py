07fd1e00f847a1d869e1602095e22b5538a36197
John Freeman <john.freeman.opensource@gmail.com>
2016-11-04 20:41:46 +0000

    Fixed for plugins with spaces in file names (#4)
    The `chown` command was failing because it was treating file names with spaces as multiple files.

--- tests/test_role.py
+++ tests/test_role.py
@@ -7,7 +7,8 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 @pytest.mark.parametrize('plugin_dir_path', [
     '/home/test_usr/.IdeaIC2016.2/config/plugins/google-java-format',
-    '/home/test_usr/.IdeaIC2016.2/config/plugins/lombok-plugin'
+    '/home/test_usr/.IdeaIC2016.2/config/plugins/lombok-plugin',
+    '/home/test_usr/.IdeaIC2016.2/config/plugins/Go'
 ])
 def test_plugins_installed(File, plugin_dir_path):
     plugin_dir = File(plugin_dir_path)
