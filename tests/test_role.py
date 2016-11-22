fa7cba8b26585428e219db676095fdfdfad74b0c
John Freeman <john.freeman.opensource@gmail.com>
2016-11-22 23:49:21 +0000

    Made plugin dir in tests dynamic
    It was hardcoded for version 2016.2.

--- tests/test_role.py
+++ tests/test_role.py
@@ -5,13 +5,17 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-@pytest.mark.parametrize('plugin_dir_path', [
-    '/home/test_usr/.IdeaIC2016.2/config/plugins/google-java-format',
-    '/home/test_usr/.IdeaIC2016.2/config/plugins/lombok-plugin',
-    '/home/test_usr/.IdeaIC2016.2/config/plugins/Go'
+@pytest.mark.parametrize('plugin_dir_name', [
+    'google-java-format',
+    'lombok-plugin',
+    'Go'
 ])
-def test_plugins_installed(File, plugin_dir_path):
-    plugin_dir = File(plugin_dir_path)
+def test_plugins_installed(Command, File, plugin_dir_name):
+    plugins_dir_pattern = '\\.IdeaIC[0-9]+\\.[0-9]/config/plugins$'
+    plugins_dir = Command.check_output('find %s | grep --color=never -E %s',
+                                       '/home/test_usr',
+                                       plugins_dir_pattern)
+    plugin_dir = File(plugins_dir + '/' + plugin_dir_name)
 
     assert plugin_dir.exists
     assert plugin_dir.is_directory
