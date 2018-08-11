03fe4ccbfbc4a4e965412772905e117fc3128eca
John Freeman <john.freeman.opensource@gmail.com>
2018-08-11 09:55:03 +0000

    Fixed Python style error in test
    Line was too long.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -34,9 +34,10 @@ def test_jar_plugin_installed(Command, File):
 
     plugins_dir = config_home + '/plugins/'
 
+    sa_plugin_pattern = 'intellij-plugin-save-actions-v[0-9\\.]+.jar'
     plugin_path = Command.check_output('find %s | grep --color=never -E %s',
                                        plugins_dir,
-                                       'intellij-plugin-save-actions-v[0-9\\.]+.jar')
+                                       sa_plugin_pattern)
 
     plugin_file = File(plugin_path)
 
